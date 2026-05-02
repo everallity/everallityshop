f347197ce97ef5e9efae675672e0234ccc84602f
Everallity <everallity@gmail.com>
2026-05-02 12:52:47 +0700

    change some config

--- src/main/java/com/everallity/ecommerce_backend/configuration/ApplicationInitConfiguration.java
+++ src/main/java/com/everallity/ecommerce_backend/configuration/ApplicationInitConfiguration.java
@@ -1,8 +1,11 @@
 package com.everallity.ecommerce_backend.configuration;
 
+import com.everallity.ecommerce_backend.constant.ErrorCode;
 import com.everallity.ecommerce_backend.entity.Account;
 import com.everallity.ecommerce_backend.entity.Role;
+import com.everallity.ecommerce_backend.exception.AppException;
 import com.everallity.ecommerce_backend.repository.AccountRepository;
+import com.everallity.ecommerce_backend.repository.RoleRepository;
 import com.everallity.ecommerce_backend.service.AccountService;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
@@ -16,13 +19,12 @@ public class ApplicationInitConfiguration {
 
 
     @Bean
-    ApplicationRunner applicationRunner(AccountRepository accountRepository, AccountService accountService, PasswordEncoder passwordEncoder) {
+    ApplicationRunner applicationRunner(AccountRepository accountRepository, AccountService accountService, PasswordEncoder passwordEncoder, RoleRepository roleRepository) {
         return args -> {
             if (accountRepository.findByEmail("admin@gmail.com").isEmpty()) {
                 Account account = new Account();
                 HashSet<Role> roles = new HashSet<>();
-                Role defaultRole = new Role();
-                defaultRole.setName("ADMIN");
+                Role defaultRole = roleRepository.findByName("ADMIN").orElseThrow(() -> new AppException(ErrorCode.INVALID_ROLE)) ;
                 roles.add(defaultRole);
                 account.setPasswordHash(passwordEncoder.encode("abcd1234"));
                 account.setRoleSet(roles);
@@ -30,6 +32,5 @@ public class ApplicationInitConfiguration {
                 accountRepository.save(account);
             }
         };
-
     }
 }
