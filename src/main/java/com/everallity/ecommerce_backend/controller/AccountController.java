b5081b645d97abf7bc8f051c40d2d343706656fe
Everallity <everallity@gmail.com>
2026-05-03 18:11:04 +0700

    update AccountController

--- src/main/java/com/everallity/ecommerce_backend/controller/AccountController.java
+++ src/main/java/com/everallity/ecommerce_backend/controller/AccountController.java
@@ -1,5 +1,7 @@
 package com.everallity.ecommerce_backend.controller;
 
+import com.everallity.ecommerce_backend.api.ApiResponse;
+import com.everallity.ecommerce_backend.entity.dto.response.TokenValidateResponseDto;
 import com.everallity.ecommerce_backend.mapper.AccountRequestDtoMapper;
 import com.everallity.ecommerce_backend.mapper.AccountResponseDtoMapper;
 import com.everallity.ecommerce_backend.entity.Account;
@@ -8,6 +10,7 @@ import com.everallity.ecommerce_backend.entity.dto.response.AccountResponseDto;
 import com.everallity.ecommerce_backend.service.AccountService;
 import jakarta.validation.Valid;
 import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
@@ -26,24 +29,35 @@ public class AccountController {
 
     @DeleteMapping("/delete")
     @Modifying
-    public Boolean deleteAccount(@RequestBody AccountRequestDto accountRequestDto) {
-        return accountService.deleteAccount(accountRequestDto);
+    public ResponseEntity<ApiResponse<Boolean>> deleteAccount(@RequestBody AccountRequestDto accountRequestDto) {
+        Boolean result = accountService.deleteAccount(accountRequestDto);
+        ApiResponse<Boolean> response = new ApiResponse<>();
+        response.setSuccess(true);
+        response.setData(result);
+        return ResponseEntity.ok(response);
     }
 
     @GetMapping
-    public List<AccountResponseDto> getAccounts() {
+    public ResponseEntity<ApiResponse<List<AccountResponseDto>>> getAccounts() {
         List<Account> accountList = accountService.getAccountList();
         List<AccountResponseDto> accountResponseDtoList = new ArrayList<>();
         for (Account account : accountList) {
             AccountResponseDto accountResponseDto = accountResponseDtoMapper.accountToAccountResponseDto(account);
             accountResponseDtoList.add(accountResponseDto);
         }
-        return accountResponseDtoList;
+        ApiResponse<List<AccountResponseDto>> apiResponse = new ApiResponse<>();
+        apiResponse.setData(accountResponseDtoList);
+        apiResponse.setSuccess(true);
+        return ResponseEntity.ok(apiResponse);
     }
 
     @GetMapping("/me")
-    public AccountResponseDto getOwnAccount(Authentication authentication) {
+    public ResponseEntity<ApiResponse<AccountResponseDto>> getOwnAccount(Authentication authentication) {
         Account account = accountService.getAccountByEmail(authentication.getName());
-        return accountResponseDtoMapper.accountToAccountResponseDto(account);
+        AccountResponseDto responseDto = accountResponseDtoMapper.accountToAccountResponseDto(account);
+        ApiResponse<AccountResponseDto> apiResponse = new ApiResponse<>();
+        apiResponse.setData(responseDto);
+        apiResponse.setSuccess(true);
+        return ResponseEntity.ok(apiResponse);
     }
 }
